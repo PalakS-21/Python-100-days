e595663b7be64ddc5162cad9d1d926c6dbd61e14
PalakS-21 <sonipalak527@gmail.com>
2026-02-22 14:47:02 +0530

    Day02

---
+++ Day03_Operators/06_comparison_operator.py
@@ -0,0 +1,19 @@
+
+#Comparison Operators : ==, !=, >, <, >=, <=
+# these compare values, and gives output as Boolean (True/False).
+
+# (==) -> checks if the vlaues are equal or not
+print(5==3) #false
+print(5==5) #true
+
+# (!=) -> not equals to, checks if two values are not equal.
+print(2!=3) #true
+print(2!=2) #false
+
+# >, < : greater than, less than
+print(2>3) #True/False
+print(2<3) #True/False
+
+# >=, <= : greter or equal, less or equal
+print(2<=8) #true
+print(2>=6) #false
---
+++ Day03_Operators/07_logical_operator.py
@@ -0,0 +1,15 @@
+
+#Logical Operators : and, or, not
+
+#and operator : both condition must be true.
+print(5 > 3 and 8 > 5) #true
+print(5 > 7 and 4 > 3) #false
+
+# or operator : retuirns true if atleast one condition is true.
+print(5 > 7 or 4 > 3) #true
+print(15 < 7 or 6 > 8) #false
+
+# not operator : flips the result, true to false and false to true.
+print(not(5>3)) #false
+print(not(3>5)) #true
+print(0 and 5)
