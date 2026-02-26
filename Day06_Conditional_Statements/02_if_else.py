c5830eaa3d20546878147d4e6f1e905d5b0b1390
PalakS-21 <sonipalak527@gmail.com>
2026-02-26 22:31:18 +0530

    if-else

--- Day06_Conditional_Statements/02_if_else.py
+++ Day06_Conditional_Statements/02_if_else.py
@@ -6,3 +6,11 @@ if age >= 18:
     print("You can Vote.")
 else:
     print("Underage. You cannot Vote.")
+print("this is out of if-else block")
+
+num = int(input("Enter a number : "))
+if (num < 50):
+    print(num, " is less than 50.")
+else:
+    print(num, " is greater than 50.")
+
