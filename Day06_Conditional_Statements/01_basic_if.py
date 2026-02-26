b18ad197535f3553dace61a6c992cb9e1473653b
PalakS-21 <sonipalak527@gmail.com>
2026-02-26 22:23:20 +0530

    conditional statements

---
+++ Day06_Conditional_Statements/01_basic_if.py
@@ -0,0 +1,15 @@
+
+# if statement - simple ccondition check.
+# if statement executes only when the condition is true.
+
+age = 18
+if age >= 18:
+    print("Eligible")
+
+x = 5
+if x > 2 :
+    print("Greater than 2")
+
+id = 145
+if id == 145:
+    print("Your id is : ", id)
---
+++ Day06_Conditional_Statements/02_if_else.py
@@ -0,0 +1,8 @@
+#if-else
+#else executes when if statement is false.
+
+age = int(input("enter your age : "))
+if age >= 18:
+    print("You can Vote.")
+else:
+    print("Underage. You cannot Vote.")
