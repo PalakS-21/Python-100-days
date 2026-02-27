8ee256eb18e5ccc2b39bc155a52313500efd73c9
PalakS-21 <sonipalak527@gmail.com>
2026-02-27 22:44:15 +0530

    elif

--- Day06_Conditional_Statements/03_if_elif.py
+++ Day06_Conditional_Statements/03_if_elif.py
@@ -36,3 +36,11 @@ elif num == 0:
 else:
     print("Number is Positive.")
 
+#movie ticket
+age = int(input("Enter your age : "))
+if age < 12:
+    print("Ticket Price : 100")
+elif age < 60:
+    print("Ticket price : 200")
+else:
+    print("Ticket price : 150")
