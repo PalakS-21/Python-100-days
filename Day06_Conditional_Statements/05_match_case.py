3ebdc9977fb89d8344c65c0a48ba7615e907b6f7
PalakS-21 <sonipalak527@gmail.com>
2026-03-03 12:33:16 +0530

    matchcase

--- Day06_Conditional_Statements/05_match_case.py
+++ Day06_Conditional_Statements/05_match_case.py
@@ -1,2 +1,18 @@
 # match checks cases from top to bottom and stops at the first matching value.
 # match-case is a cleaner alternative to if-elif for matching fixed values.
+x= int(input("Enter value of x : "))
+# x is a variable to match
+match x:
+    # if x is 0
+    case 0:
+        print("x is zero")
+        # case with if condition
+    case 4:
+        print("case is 4")
+    case _ if x!=50:
+        print("x is not 50")
+    case _ if x!=100:
+        print("x is not 100")
+        # default case -> else 
+    case _:
+        print("x is", x)    
