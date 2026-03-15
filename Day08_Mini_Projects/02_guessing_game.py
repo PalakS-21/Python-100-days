091d9d16b625668d87ad9cf4857a6fea6a1bda58
PalakS-21 <sonipalak527@gmail.com>
2026-03-15 20:16:22 +0530

    mini game

---
+++ Day08_Mini_Projects/02_guessing_game.py
@@ -0,0 +1,19 @@
+
+# Guess the number game...in 5 attempts.
+
+secret_num = 8
+
+for i in range(5): 
+    guess = int(input("Guess the secret number: "))
+
+    if guess == secret_num:
+        print("Woww!! You guessed it Correct 💪")
+        break
+
+    elif guess < secret_num:
+        print("Too small. Try again 😁")
+
+    else:
+        print("Too big!! 😶")    
+
+print("\nNo attempts left. You have used all the 5 attempts 🫠")
