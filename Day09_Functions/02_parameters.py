3b3e60467509b6fbbada3b752a24a85c6dfe7a83
PalakS-21 <sonipalak527@gmail.com>
2026-03-16 23:37:35 +0530

    parameters

--- Day09_Functions/02_parameters.py
+++ Day09_Functions/02_parameters.py
@@ -1,3 +1,14 @@
 
 # parameters allow functions to accept values and work with different inputs.
+# a aparameter is a variable inside the function that receives a value.
 
+def greet(name):
+    # here name is parameter
+    print("Hello", name)
+
+# calling the function with argument
+greet('Palak') 
+# calling the function multiple times  
+greet("Harry Potter") 
+greet('Alexa')
+greet("Steve")
