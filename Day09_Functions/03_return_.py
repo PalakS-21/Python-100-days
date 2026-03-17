618032699665aaf3a44280438e10cbc37dcafb15
PalakS-21 <sonipalak527@gmail.com>
2026-03-17 23:43:38 +0530

    return statement

--- Day09_Functions/03_return_.py
+++ Day09_Functions/03_return_.py
@@ -13,15 +13,40 @@ print(result + result1)
 
 # with print, result cannot be reused
 def add(a, b):
-    print(a + b)
+    print(a + b)  # 8
 result = add(5, 3)    
-print(result)
+print(result)   # None because function did not send back the result.
 
 
-# return stops the function
+# return ends the function
 
 def test():
     print("before return!")
     return
     print("Byee!")
 test()    
+
+# even - odd checking with return
+
+num = int(input("Enter a number : "))
+
+def check_even(num):
+
+    if num %2 == 0:
+        return "Even"
+    if num %2 != 0:
+        return "Odd"
+
+print(check_even(num))    
+
+
+# checking eligibity
+
+age = int(input("Enter a number : "))
+def check_eligibility(age):
+    if age >= 18:
+        return True
+    if age < 18:
+        return False
+
+print("Eligible : ", check_eligibility(age))    
