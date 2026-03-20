fe2250c00627a567486a95ff1d365aaa4de06d65
PalakS-21 <sonipalak527@gmail.com>
2026-03-20 20:30:12 +0530

    shopping cart

---
+++ Day10_Lists/05_shoppingCart.py
@@ -0,0 +1,34 @@
+# Shopping cart mini project
+
+cart = []
+def add_to_cart():
+    item = input("Enter item : ")
+    cart.append(item)
+    print("Item added to Cart!!")
+
+def remove_item():
+    item = input("Enter item to  remove: ")
+    if item in cart:
+        cart.remove(item)
+        print("Item removed!")
+    else:
+        print("Item not found!")
+
+def show_cart():
+    print("Cart items : ", cart)
+
+while True:
+    print("\n1. Add\n2. Remove\n3. Show\n4. Exit")
+    ch = input("Choice : ")
+
+    if ch == '1':
+        add_to_cart()
+
+    elif ch == '2':
+        remove_item()
+
+    elif ch == '3':
+        show_cart()
+
+    elif ch == '4':
+        break
