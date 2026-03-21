48cb860c26680abdeb8a7ec3ff5abce6214ea5e6
PalakS-21 <sonipalak527@gmail.com>
2026-03-22 00:01:38 +0530

    tuples

--- Day11_Tuples/01_basic_tuples.py
+++ Day11_Tuples/01_basic_tuples.py
@@ -8,8 +8,11 @@ print(tup)
 
 tup = (1,) # comma(,) after one element is mandatory
 print(tup)
-print(type(tup))
+print(type(tup))  # tuple
 
-# without comma(,) the type changes
+# without comma(,) the type changes to int
 # example
 
+tup = (5)
+print(type(tup))  # int
+print(tup)
