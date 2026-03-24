8fa5e3cfbe6440c3d15f09751d87889b72c46a2a
PalakS-21 <sonipalak527@gmail.com>
2026-03-24 23:50:37 +0530

    tuple projects

---
+++ Day11_Tuples/03_mini_projects.py
@@ -0,0 +1,10 @@
+
+# bus seat booking view, where seat are fixed.
+
+seats = ("A1", "A2", "A3", "A4")
+
+def show_seats():
+    for seat in seats:
+        print("Seat :", seat)
+
+show_seats()        
