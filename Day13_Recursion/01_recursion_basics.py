eed9b0847afda2b98714aa82d37af55da35c9518
PalakS-21 <sonipalak527@gmail.com>
2026-03-26 20:59:05 +0530

    recursion

---
+++ Day13_Recursion/01_recursion_basics.py
@@ -0,0 +1,20 @@
+
+# recursion -> when a function calls itself.
+
+# factorial(5) - 5*4*3*2*1
+# factorial(4) - 4*3*2*1
+# factorial(3) - 3*2*1
+# factorial(2) - 2*1
+# factorial(1) - 1
+
+# factorial(n) = n * factorial(n - 1)
+
+def factorial(n):
+    if (n == 0 or n == 1):
+        return 1
+    else:
+        return n * factorial(n - 1) # function calling itself
+    
+print(factorial(5)) 
+print(factorial(4))  
+print(factorial(1)) 
