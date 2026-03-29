51693131d2017501f36319cb897bf20009461d16
PalakS-21 <sonipalak527@gmail.com>
2026-03-29 23:58:50 +0530

    sets

--- Day14_Sets/02_sets_methods.py
+++ Day14_Sets/02_sets_methods.py
@@ -5,5 +5,9 @@ set2 = {2, 4, 5, 7, 9, 55}
 
 print(set1.union(set2))
 
-print(set1, set2)
+print(set1, set2) # not changed, untouched
+
+# update -> update set1 with set 2 value
+set1.update(set2)
+print(set1)
 
