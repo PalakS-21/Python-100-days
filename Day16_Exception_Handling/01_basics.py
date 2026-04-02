ccb2fafaedb5f4f9c808b57b1f57a96e01ce1cb7
PalakS-21 <sonipalak527@gmail.com>
2026-04-02 23:55:06 +0530

    learn

---
+++ Day16_Exception_Handling/01_basics.py
@@ -0,0 +1,10 @@
+#  exception handling -> 
+
+a = input("Enter the number :")
+print(f"Multiplication table of {a} is :")
+
+for i in range(1, 11):
+    print(f"{int(a)} X {i} = {int(a)*i}")
+
+print("Some imp line of code")
+print("Byee!!!")
