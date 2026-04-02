1f0f125600fabb642d0f66978efbfce6e0db1a92
PalakS-21 <sonipalak527@gmail.com>
2026-04-02 23:56:44 +0530

    learn

--- Day16_Exception_Handling/01_basics.py
+++ Day16_Exception_Handling/01_basics.py
@@ -5,6 +5,7 @@ print(f"Multiplication table of {a} is :")
 
 for i in range(1, 11):
     print(f"{int(a)} X {i} = {int(a)*i}")
+# if input is wrong(ex : ps -> input), program will halt here
 
 print("Some imp line of code")
 print("Byee!!!")
