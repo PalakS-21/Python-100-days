3066285735651f0f6e023cf3b632d04139dae3ff
PalakS-21 <sonipalak527@gmail.com>
2026-04-02 23:59:57 +0530

    code

--- Day16_Exception_Handling/01_basics.py
+++ Day16_Exception_Handling/01_basics.py
@@ -1,11 +1,28 @@
 #  exception handling -> 
 
+# a = input("Enter the number :")
+# print(f"Multiplication table of {a} is :")
+
+# for i in range(1, 11):
+#     print(f"{int(a)} X {i} = {int(a)*i}")
+# # if input is wrong(ex : ps -> input), program will halt here
+
+# print("Some imp line of code")
+# print("Byee!!!")
+
+
+# with exception handling
+
 a = input("Enter the number :")
 print(f"Multiplication table of {a} is :")
 
-for i in range(1, 11):
-    print(f"{int(a)} X {i} = {int(a)*i}")
-# if input is wrong(ex : ps -> input), program will halt here
+try:
+    for i in range(1, 11):
+        print(f"{int(a)} X {i} = {int(a)*i}")
+except Exception as e:
+#  print(e) 
+   print("Sorry, some error occured!!")       
+
 
 print("Some imp line of code")
 print("Byee!!!")
