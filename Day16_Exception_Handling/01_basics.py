c0d9cfef44471f4724d9c2989ac81a32e6f24ece
PalakS-21 <sonipalak527@gmail.com>
2026-04-08 02:33:30 +0530

    exception handling

--- Day16_Exception_Handling/01_basics.py
+++ Day16_Exception_Handling/01_basics.py
@@ -21,7 +21,7 @@ try:  # code thaat may cause error
         print(f"{int(a)} X {i} = {int(a)*i}")
 
 except Exception as e:  # what to do if error occurs
-#  print(e) # error print
+#  print(e) # prints actual error 
    print("Sorry, some error occured!!")       
 
 
@@ -32,7 +32,7 @@ print("\n")
 #---------------------------------------------------
 
 try:
-    num = int(input("Enteer a number :"))
+    num = int(input("Enter a number :"))
     print(10/num)
 # if num = 0,     
 except:  
@@ -48,4 +48,33 @@ try:
 except ZeroDivisionError:
     print("Cannot be divided by zero!")
 except ValueError:
-    print("Invalid Input!")    
+    print("Invalid Input!\n")    
+
+#------------------------------------------------
+
+try:
+    num1 = int(input("Enter first number :"))
+    num2 = int(input("Enter second number :"))
+    print(num1 / num2)
+
+except ZeroDivisionError:
+        print("Cannot be divided by zero! ")
+except ValueError:
+    print("Invalid Input!")
+
+#------------------------------------------------
+
+# Index error
+lst = [10, 20, 30, 40]
+
+try:
+    index  = int(input("Enter index :"))
+    print("Value is :", lst[index]) 
+
+except IndexError:
+    print("Index out of range!")
+
+except ValueError:
+    print("Invalid input. Please enter a Valid input.")   
+
+#-----------------------------------------------------------
