3d75659d55efa97bf15cea4aea6ead323e9381b5
PalakS-21 <sonipalak527@gmail.com>
2026-04-08 00:52:48 +0530

    exception

--- Day16_Exception_Handling/01_basics.py
+++ Day16_Exception_Handling/01_basics.py
@@ -1,4 +1,4 @@
-#  exception handling -> 
+#  exception  -> a error that occurs when your program is runnning. 
 
 # a = input("Enter the number :")
 # print(f"Multiplication table of {a} is :")
@@ -16,13 +16,36 @@
 a = input("Enter the number :")
 print(f"Multiplication table of {a} is :")
 
-try:
+try:  # code thaat may cause error
     for i in range(1, 11):
         print(f"{int(a)} X {i} = {int(a)*i}")
-except Exception as e:
-#  print(e) 
+
+except Exception as e:  # what to do if error occurs
+#  print(e) # error print
    print("Sorry, some error occured!!")       
 
 
 print("Some imp line of code")
 print("Byee!!!")
+print("\n")
+
+#---------------------------------------------------
+
+try:
+    num = int(input("Enteer a number :"))
+    print(10/num)
+# if num = 0,     
+except:  
+    print("Error occurerd!")
+
+#----------------------------------------------------
+
+# specific error
+
+try:
+    num = int(input("Enter a number : "))
+    print(10/num)
+except ZeroDivisionError:
+    print("Cannot be divided by zero!")
+except ValueError:
+    print("Invalid Input!")    
