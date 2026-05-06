9dc6f5601d635e6e69f624637f23fe264edc8b6e
PalakS-21 <sonipalak527@gmail.com>
2026-05-06 15:24:30 +0530

    finally

--- Day16_Exception_Handling/02_finally_clause.py
+++ Day16_Exception_Handling/02_finally_clause.py
@@ -5,3 +5,20 @@ try:
     print("hello")
 finally:
     print("This always runs.")
+
+# if error occurs
+# try:
+#     print(10 / 0)
+# finally:
+#     print("This still runs.") 
+#     #throws error after this
+
+try:
+    num = int(input("Enter a number :"))
+    print(10 / num)
+
+except ZeroDivisionError:
+    print("Cannot be divided by zero.")
+
+finally:
+    print("Program finished.")
