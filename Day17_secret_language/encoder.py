e80f097547eb2d7b8390e718d6d87c83e88c30f9
PalakS-21 <sonipalak527@gmail.com>
2026-05-28 18:24:22 +0530

    encode_decode

---
+++ Day17_secret_language/encoder.py
@@ -0,0 +1,21 @@
+# encoding:
+# if the word contains atleast 3 characters, remove the first letter and append it at the end
+# now append three random characters at the starting and the end
+# else:
+#  simply reverse the string
+
+def encode(word):
+
+    if len(word) >= 3:
+
+        secret = "xyz" + word[1:] + word[0] + "abc"
+        # ex : word = harry
+        # word[1:] -> arry
+        # word[1:] + word[0] -> arry + h -> arryh
+
+        return secret
+
+    else:
+        return word[::-1]
+
+    # word[::-1] -> yrrah (reversed)
---
+++ Day17_secret_language/main.py
@@ -0,0 +1,23 @@
+# importing encoder and decoder in main.py
+
+from encoder import encode
+from decoder import decode
+
+print("Secret Language Encoder and Decdoder")
+
+choice = input("Type 'e' to encode or 'd' to decode: ").lower()
+
+word = input("Enter word: ")
+
+if choice == 'e':
+    
+    result = encode(word)
+    print("Encoded word: ", result)
+
+elif choice == 'd':
+
+    result = decode(word)
+    print("Decoded word: ", result)
+
+else:
+    print("Invalid chouice!")
