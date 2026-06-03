77b2d96d18d273dee4c9274f64081157a028310f
PalakS-21 <sonipalak527@gmail.com>
2026-06-03 23:47:23 +0530

    import

--- Day21_Import/main.py
+++ Day21_Import/main.py
@@ -1,7 +1,19 @@
 import math as m
 import hello
+import random
+import datetime
+
 
 print(m.sqrt(9))
+print(m.pi)
+print("\n")
+
 
 # print("Main file running")
 hello.greet()
+
+print(random.randint(1, 100))
+print("\n")
+
+print(datetime.date.today())
+
