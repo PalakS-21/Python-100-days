d0e43024313c952f1c7eadf89392e9ddfeb2c31e
PalakS-21 <sonipalak527@gmail.com>
2026-06-05 23:07:31 +0530

    os module

---
+++ Day23_OS_Module/Projects/FolderGenerator.py
@@ -0,0 +1,28 @@
+import os
+
+# for i in range(1, 101):
+#     folder = f"Day{i:02d}"
+
+#     if not os.path.exists(folder):
+#         os.rmdir(folder)
+
+# print("100 folders deleted")
+
+
+# deleting 100 folders (non-empty)
+
+import os
+import stat
+
+for i in range(1, 101):
+    folder = f"Day{i:02d}"
+    if os.path.exists(folder) and os.path.isdir(folder):
+        # Remove read-only attribute if present
+        os.chmod(folder, stat.S_IWRITE)
+
+        # Delete only if empty
+        if not os.listdir(folder):
+            os.rmdir(folder)
+            print(f"{folder} deleted")
+        else:
+            print(f"{folder} kept (not empty)")
--- Day23_OS_Module/os_module.py
+++ Day23_OS_Module/os_module.py
@@ -6,7 +6,33 @@ print(os.getcwd())
 # listdir() -> list all files and folders
 print(os.listdir())
 
+# chdir() -> change directory
+os.chdir("Day23_OS_Module")
+print(os.getcwd())
+
 # mkdir() -> create a folder (only one folder)
-os.mkdir("Day23_OS_Module/Projects")
+# os.mkdir("Day23_OS_Module/Projects")
 
 # makedirs()-> creates multiple folder
+# os.makedirs("Day23_OS_Module/Projects/Beginner")
+
+# rename folder -> rename()
+# os.rename("Projects", "MiniProjects")
+
+# print(os.listdir("MiniProjects"))
+
+# emdir() -> delete empty folder, folder must be empty
+# os.rmdir("MiniProjects") # if not empty -> OSError
+
+# print(os.getcwd())
+
+# check if file exists
+print(os.path.exists("os_module.py"))
+
+# check if folder exists
+print(os.path.exists("Projects"))
+
+# get absolute path
+print(os.path.abspath("os_module.py"))
+
+os.makedirs("Projects")
