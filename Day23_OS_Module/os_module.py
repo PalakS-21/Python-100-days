bfdc83deb8ff3285bd29883ab40cbc353caba255
PalakS-21 <sonipalak527@gmail.com>
2026-06-05 22:05:16 +0530

    os module

---
+++ Day23_OS_Module/os_module.py
@@ -0,0 +1,12 @@
+import os
+
+# getcwd() -> Current Working Directory ("The folder where Python is currently running.")
+print(os.getcwd())
+
+# listdir() -> list all files and folders
+print(os.listdir())
+
+# mkdir() -> create a folder (only one folder)
+os.mkdir("Day23_OS_Module/Projects")
+
+# makedirs()-> creates multiple folder
