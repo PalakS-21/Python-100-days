0f3f0a4a057a553ed99570cf6392771aad6bbeba
PalakS-21 <sonipalak527@gmail.com>
2026-06-07 23:17:30 +0530

    notes

--- Day25_File_IO/main.py
+++ Day25_File_IO/main.py
@@ -2,7 +2,7 @@
 
 file = open('mynotes.txt', 'r')
 
-# reading a file -> read()
+# reading a file -> read(), mode -> 'r'
 
 text = file.read()
 
@@ -16,11 +16,11 @@ file.close()
 # writing to file -> write()
 # Mode "w" erases old content.
 
-file = open('mynotes.txt', 'w')
+# file = open('mynotes.txt', 'w')
 
-file.write("Hello World")
+# file.write("Hello World")
 
-file.close()
+# file.close()
 
 # append mode -> 'a' -> keeps old data
 
@@ -29,3 +29,42 @@ file = open('mynotes.txt', 'a')
 file.write("\nAppend Mode")
 
 file.close()
+
+# read line by line
+
+file  = open('mynotes.txt')
+
+for line in file:
+    print(line)
+
+file.close()
+
+# readline() -> only first line is read.
+
+file = open("mynotes.txt")
+
+print(file.readline())
+
+file.close()
+
+# readlines() -> returns a list.
+
+file = open("mynotes.txt")
+
+print(file.readlines())
+
+# better code
+
+file.close()
+
+with open('mynotes.txt') as file:
+    content = file.read()
+
+print(content)
+
+# notes app example
+
+note = input("Enterr note:")
+
+with open('mynotes.txt', 'a') as file:
+    file.write(note + "\n")
---
+++ Day25_File_IO/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+f = open('mynotes.txt', 'r')
+
+# text = f.read()
+
+# print(text)
+# f.close()
