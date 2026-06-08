da1236a14b64839a2524d6e269af449c287b11c2
PalakS-21 <sonipalak527@gmail.com>
2026-06-08 20:20:38 +0530

    lambda funtion

---
+++ Day26_Lambda_Functions/lambda_func.py
@@ -0,0 +1,14 @@
+# lambda function is a small anonymous function.
+
+# normal function
+
+# def square(x):
+#     return x * x
+
+# print(square(5))
+
+# with lambda function
+
+square = lambda x: x * x
+
+print(square(5))
