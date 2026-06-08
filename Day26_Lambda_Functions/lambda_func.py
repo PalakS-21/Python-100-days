6e62362730927965cf969ff47c974dfde96a0a76
PalakS-21 <sonipalak527@gmail.com>
2026-06-08 20:23:57 +0530

    practice

--- Day26_Lambda_Functions/lambda_func.py
+++ Day26_Lambda_Functions/lambda_func.py
@@ -8,7 +8,21 @@
 # print(square(5))
 
 # with lambda function
+# syntax -> lambda arguments : expression
 
+# square of a number
 square = lambda x: x * x
 
 print(square(5))
+
+# add 2 numbers
+
+add = lambda a, b: a + b
+
+print(add(5, 4))
+
+# find maximum of 2 nums
+
+max = lambda a, b: a if a > b else b
+
+print(max(55, 8))
