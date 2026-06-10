51eadd2c812a33a80ef5f8dafb91afca05a4fed7
PalakS-21 <sonipalak527@gmail.com>
2026-06-10 22:38:42 +0530

    class and object

--- Day28_OOP_Part1/class_and_object.py
+++ Day28_OOP_Part1/class_and_object.py
@@ -1,3 +1,26 @@
+# class -> Blueprint / template for creating an object.
+
+class Student:
+    pass # nothing for bnow
+
+# creating an object 
+s1 = Student() # s1 -> object, student -> creating object
+print(s1)
+
+# adding attributes
+s1.name = "Steve"
+s1.age = 20
+
+ # accessing the attributes
+print(s1.name)
+print(s1.age)
+
+# multiple objects
+s2 = Student()
+s3 = Student()
+
+
+# with self
 class Person:
     name = "Palak"
     occupation = "Software Engineer"
