245d1215ba4e4442f105f1d16a46bd4042569425
PalakS-21 <sonipalak527@gmail.com>
2026-06-10 22:25:31 +0530

    class and object

--- Day28_OOP_Part1/class_and_object.py
+++ Day28_OOP_Part1/class_and_object.py
@@ -2,7 +2,24 @@ class Person:
     name = "Palak"
     occupation = "Software Engineer"
 
+    def info(self):
+        print(f"{self.name} is a {self.occupation}.")
+        # self -> current object being createed
+
 a = Person() # object
+b = Person()
+
 # a.name = "Ron"
 # a.occupation = "HR"
 print(a.name, a.occupation)
+
+a.info()
+
+a.name = "harry"
+a.occupation = "Accountant"
+
+b.name = "Ritika"
+b.occupation = "Dancer"
+
+a.info()
+b.info()
