d3155308da192e9c1590c7d7a6cb14b2a190a11d
PalakS-21 <sonipalak527@gmail.com>
2026-06-15 20:21:09 +0530

    constructor

---
+++ Day28_OOP_Part1/constructor.py
@@ -0,0 +1,43 @@
+# constructor -> special method that runs automatically when an object is created.
+
+class Student:
+
+    def __init__(self):
+        print("Student object created")
+
+
+s1 = Student()
+
+# constructor runs automatically.
+
+# custructor with parameters
+
+class Student:
+
+    def __init__(self, name):
+        print(name)
+
+s1 = Student("Riya")
+
+# multiple paramerters
+
+class Student:
+
+    def __init__(self, name, age):
+        print(name)
+        print(age)
+
+s1 = Student("Harry", 21)
+
+# saving Data in object
+
+class Student:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+s1 = Student("Palak", 20)
+
+print(s1.name)
+print(s1.age)
