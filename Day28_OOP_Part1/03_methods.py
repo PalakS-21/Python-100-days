0b1c6e6942cd2cd024d715ca9ffe61c7bd5993a9
PalakS-21 <sonipalak527@gmail.com>
2026-06-17 00:10:57 +0530

    methods

---
+++ Day28_OOP_Part1/03_methods.py
@@ -0,0 +1,43 @@
+# method -> a function defined inside a class.
+
+class Student:
+
+    def greet(self):
+        print("Hello!")
+
+s1 = Student()
+
+s1.greet()
+
+# with constructor
+
+class Student:
+
+    def __init__(self, name):
+        self.name = name
+    
+    def greet(self):
+        print("Hello,", self.name)
+
+s1 = Student("Harry Potter")
+s2 = Student("Hermoine")
+s3 = Student("Ron")
+
+s1.greet()
+s2.greet() # multiple objects
+s3.greet()
+
+# bankAccount
+
+class BankAccount:
+
+    def __init__(self, balance):
+        self.balance = balance
+
+    def show_balance(self):
+        print("Balance =", self.balance)
+
+account = BankAccount(100000)
+
+account.show_balance()
+
