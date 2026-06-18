6bef9047f96107816815a84f7892af459f0c61ef
PalakS-21 <sonipalak527@gmail.com>
2026-06-19 00:00:04 +0530

    decorators

--- Day28_OOP_Part1/04_decorators.py
+++ Day28_OOP_Part1/04_decorators.py
@@ -17,3 +17,25 @@ greet()
 # 2. wrapper() adds extra code
 # 3. return wrapper
 # 4. @decorator applies it
+
+# LOGIN SYSTEM
+
+
+def login_required(func):
+
+    def wrapper():
+        print("Checking Login...")
+        func()
+    
+    return wrapper
+
+@login_required
+def view_profile():
+    print("Profile Opened")
+
+@login_required
+def edit_profile():
+    print("Profile Edited")
+
+view_profile()
+edit_profile()
