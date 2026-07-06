2960de1fdd159c71d60d4ec1a4ca08231b5f9237
PalakS-21 <sonipalak527@gmail.com>
2026-07-06 23:56:14 +0530

    polymorphism

--- Day29_OOP_Part2/03_polymorphism.py
+++ Day29_OOP_Part2/03_polymorphism.py
@@ -1,5 +1,5 @@
 # POLYMORPHISM -> Polymorphism allows the same method or function name to perform different tasks depending on the object.
-
+# method overriding -> same name, different objects
 class Person:
 
     def introduce(Self):
@@ -15,3 +15,20 @@ s1 = Student()
 
 p1.introduce()
 s1.introduce()
+
+# Method Overloading -> multiple methods with same name but with different parameters in the same class.
+
+
+class Calculator:
+
+    def add(Self, a, b):
+        print(a + b)
+
+    def add(self, a, b, c):
+        print(a + b + c)
+ 
+c1 = Calculator()
+
+c1.add(10, 20, 30)
+# c1.add(10, 2) # TypeError : missing 'c
+
