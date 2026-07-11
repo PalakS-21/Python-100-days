65603ec221fe6ed3d7ec8d44b314a32562662201
PalakS-21 <sonipalak527@gmail.com>
2026-07-11 21:50:44 +0530

    polymorphism

--- Day29_OOP_Part2/03_polymorphism.py
+++ Day29_OOP_Part2/03_polymorphism.py
@@ -16,8 +16,8 @@ s1 = Student()
 p1.introduce()
 s1.introduce()
 
-# Method Overloading -> multiple methods with same name but with different parameters in the same class.
 
+# Method Overloading -> multiple methods with same name but with different parameters in the same class.
 
 class Calculator:
 
@@ -32,3 +32,28 @@ c1 = Calculator()
 c1.add(10, 20, 30)
 # c1.add(10, 2) # TypeError : missing 'c
 
+# methods to achieve method overloading in python using c = 0 or *args
+class Calculator:
+
+    def add(self, a, b, c = 0): 
+        print(a + b + c)
+
+c2 = Calculator()
+
+c2.add(44, 66)
+c2.add(22, 22, 33)
+
+# using *args
+
+class Calculator:
+
+    def add(self, *numbers):  # *numbers accepts any  number of arguments.
+        print(sum(numbers))
+
+c3 = Calculator()
+
+c3.add(11, 11)
+
+c3.add(5, 5, 5)
+
+c3.add(60, 40, 100, 55)
