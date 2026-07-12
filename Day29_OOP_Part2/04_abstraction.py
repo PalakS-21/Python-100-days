22c3593bf51faca5a48242966c3d73f63a22b03d
PalakS-21 <sonipalak527@gmail.com>
2026-07-12 23:16:44 +0530

    abstraction

--- Day29_OOP_Part2/04_abstraction.py
+++ Day29_OOP_Part2/04_abstraction.py
@@ -2,9 +2,14 @@
 
 from abc import ABC, abstractmethod # to use abstraction
 
-# abc -> abstract base class
+# abc -> abstract base class -> module/library
+# ABC -> class inside abc module
+# abstractmethod -> Decorator inside the abc module -> @abstractmethod
 
-class Animal(ABC):
+class Animal(ABC):  # Animal inherits from ABC
+
+    def eat(self): # can have normal class
+        print("Eating")
 
     @abstractmethod # every child must write this method
     def sound(self):
@@ -12,10 +17,85 @@ class Animal(ABC):
 
 class Dog(Animal):
 
-    # def sound(self):
-        # print("Bark")
-        pass
+    def sound(self):
+        print("Bark")
+        # pass
 
 d1 = Dog()
 
-# d1.sound()
+d1.sound()
+
+# ABC makes the class abstract.
+# @abstractmethod makes the method abstract, and prevents object creation until child classes implement thet method.
+
+# if a class inherits from ABC but has no @abstractmethod , then also it is an abstract class.
+from abc import ABC
+
+class Animal(ABC):
+    pass
+
+a1 = Animal()   # Allowed
+
+
+# @abstractmethod prevents object creation until child classes implement thet method.
+
+# from abc import ABC, abstractmethod
+
+# class Vehicle(ABC):
+     
+#      @abstractmethod
+#      def Car(self):    
+#         pass
+
+# v1 = Vehicle()
+
+#---------------------------------------------------------------------
+
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+     
+     @abstractmethod
+     def start(self):    
+        pass
+
+# implementation
+class Toyota(Vehicle): # Toyota is inherited from Vehicle
+
+    def start(self):
+        print("start")
+    
+    def drive(self):
+        print("Driving Fortuner")
+
+v1 = Toyota()
+v1.start()
+v1.drive()
+
+# In an abstract class, the child class must implement all abstract methods with the same name and compatible parameters.
+
+# Payment App Example
+
+from abc import ABC, abstractmethod
+
+class Payment(ABC):
+
+    @abstractmethod
+    def pay(self):
+        pass
+
+class UPI(Payment):
+
+    def pay(self):
+        print("Payment through UPI")
+
+class CreditCard(Payment):
+
+    def pay(self):
+        print("Payment through Credit Card")
+
+p1 = UPI()
+# p2 = CreditCard()
+
+p1.pay()
+# p2.pay()
