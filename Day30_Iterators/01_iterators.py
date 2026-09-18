9d0eacc53043f33f3e82fc18104aa9f90b8e764a
PalakS-21 <sonipalak527@gmail.com>
2026-09-18 23:41:25 +0530

    iterators

--- Day30_Iterators/01_iterators.py
+++ Day30_Iterators/01_iterators.py
@@ -10,7 +10,16 @@ print(next(my_iter)) #10
 print(next(my_iter)) #20
 print(next(my_iter)) #30
 print(next(my_iter)) #40
+print(next(my_iter)) #StopIteration 
+
 
 # basically, iter() acts as a pointer, that points at the first number/element, and then moves to next element sequentially.
 # iter() creates an iterator from list.
 # next() means the next available element.
+
+print("\n")
+
+# and thats how loop works internally.
+
+for num in numbers:
+    print(num)
